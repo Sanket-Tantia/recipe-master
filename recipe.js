987cf7a95bfe6ceae904e948be772ac293b1e779
Tantia <satantia@teksystems.com>
2019-09-23 03:11:46 +0530

    Initial commit

---
+++ recipe.js
@@ -0,0 +1,122 @@
+const {
+    dialogflow,
+    Suggestions,
+    BasicCard,
+    Button,
+    Image,
+    BrowseCarouselItem,
+    BrowseCarousel
+} = require('actions-on-google');
+
+// Instantiate the Dialogflow client.
+const app = dialogflow({ debug: true });
+const express = require('express')
+const request = require("request");
+const bodyParser = require('body-parser')
+
+var recipe_search_http_implementation = function(query) {
+    return new Promise((resolve, reject) => {
+        var options = {
+            method: 'GET',
+            url: 'https://api.edamam.com/search',
+            qs: {
+                q: query,
+                app_id: '105c9de2',
+                app_key: 'ed97a2297018f1a0e6fce2f1a8ac42e2',
+                from: '0',
+                to: '6'
+            }
+        };
+
+        request(options, function(error, response, body) {
+            if (error) reject(error);
+            body = JSON.parse(body);
+            resolve(body.hits)
+        });
+    })
+}
+
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'
+
+app.intent('Default Welcome Intent', async function(conv) {
+    conv.ask(`Hello! Welcome to Recipe Master! Tell me the dish name so that I can get you the recipes?`);
+    conv.ask(new Suggestions('Hakka noodles', 'Cheese cake', `Grilled chicken`, 'Sunny side up'));
+});
+
+app.intent('actions_intent_CANCEL', async function(conv) {
+    conv.user.storage = {};
+    conv.close(`I hope you liked my recipes. Looking forward to serve you again. Bye!`);
+});
+
+app.intent('getrecipe', async function(conv, { any }) {
+
+    // if (!conv.screen) {
+    //     conv.ask('Sorry, try this on a screen device or select the ' +
+    //         'phone surface in the simulator.');
+    //     conv.ask('Which response would you like to see next?');
+    //     return;
+    // }
+
+    hits = await recipe_search_http_implementation(any);
+
+    if (hits.length == 0) {
+        conv.ask("Sorry I could not find any recipe. Please enter another dish name.")
+
+    } else if (hits.length == 1) {
+        singleRecipe = hits[0]
+        conv.ask(`Here are your recipes.`);
+        conv.ask(new BasicCard({
+            text: `Health Labels: ${singleRecipe.recipe.healthLabels.join(", ")}\nCalories: ${singleRecipe.recipe.calories}`, // Note the two spaces before '\n' required for
+            // a line break to be rendered in the card.
+            subtitle: `Source: ${singleRecipe.recipe.source}`,
+            title: singleRecipe.recipe.label,
+            buttons: new Button({
+                title: `Full recipe`,
+                url: singleRecipe.recipe.url,
+            }),
+            image: new Image({
+                url: singleRecipe.recipe.image,
+                alt: `Image for ${any}`,
+            }),
+            display: 'CROPPED',
+        }));
+        conv.ask('Which recipe would you like to see next?');
+        conv.ask(new Suggestions('Hakka noodles', 'Cheese cake', `Grilled chicken`, 'Sunny side up'));
+
+    } else {
+        conv.ask(`Here are your recipes.`);
+        carouselItems = []
+        hits.forEach(eachRecipe => {
+            carouselItems.push(new BrowseCarouselItem({
+                title: eachRecipe.recipe.label,
+                url: eachRecipe.recipe.url,
+                description: `Diet Labels: ${eachRecipe.recipe.healthLabels.join(", ")}\nCalories: ${eachRecipe.recipe.calories}`,
+                image: new Image({
+                    url: eachRecipe.recipe.image,
+                    alt: `Image for ${any}`,
+                }),
+                footer: `Source: ${eachRecipe.recipe.source}`,
+            }))
+        });
+        conv.ask(new BrowseCarousel({
+            items: carouselItems,
+        }));
+        conv.ask('Which recipe would you like to see next?');
+        conv.ask(new Suggestions('Hakka noodles', 'Cheese cake', `Grilled chicken`, 'Sunny side up'));
+    }
+});
+
+
+const expressApp = express().use(bodyParser.json())
+
+expressApp.get('/', (req, res) => res.send('online'))
+expressApp.post('/dialogflow', app)
+
+try {
+    expressApp.listen(process.env.PORT || 5000)
+} catch (error) {
+    console.log(error)
+}
+
+console.log(`Listening on port ${process.env.PORT || 5000}`)
